0de6ed948e8764444c1dba3ff59f7e106aa75a67
MINSsss96 <minsue0422@naver.com>
2025-08-27 11:04:03 +0900

    퀴즈 완성

---
+++ src/main/java/com/my/jpaTest/entity/Entertainment.java
@@ -0,0 +1,26 @@
+package com.my.jpaTest.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Entertainment {
+    @Id
+    private String enterId;
+    private String enterName;
+    @OneToMany(mappedBy = "entertainment",fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @Builder.Default
+    List<GirlGroup> groups = new ArrayList<>();
+}
