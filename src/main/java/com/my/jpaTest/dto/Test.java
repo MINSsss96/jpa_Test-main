511283d418fd214bbe5f3603ca27d92e0b749064
MINSsss96 <minsue0422@naver.com>
2025-08-28 09:53:38 +0900

    1. Test 생성 2. Getter, Setter 만듦

---
+++ src/main/java/com/my/jpaTest/dto/Test.java
@@ -0,0 +1,14 @@
+package com.my.jpaTest.dto;
+
+import jakarta.persistence.GeneratedValue;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Test {
+    private Long id;
+    private String name;
+
+}
